e6dbcdaa568d04d695b8766f90ae23a475f5639b
ellenjane001 <ellenjane.000001998@gmail.com>
2023-02-05 11:31:49 +0800

    added logs table

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Authentication\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Inventory\InventoryController;
-use App\Http\Controllers\Transaction\TransactionsController;
+use App\Http\Controllers\Transaction\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +20,13 @@ use App\Http\Controllers\Transaction\TransactionsController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:api')->controller(TransactionsController::class)->group(function () {
-    Route::post('/transactions/fetch', 'fetch');
-    Route::post('/transactions/add', 'add');
+Route::middleware('auth:api')->controller(TransactionController::class)->group(function () {
+    Route::post('transactions/fetch', 'fetch');
+    Route::post('transactions/add', 'add');
 });
 Route::middleware('auth:api')->controller(InventoryController::class)->group(function () {
-    Route::post('/inventory/fetch', 'fetch');
-    Route::post('/inventory/add', 'add');
+    Route::post('inventory/fetch', 'fetch');
+    Route::post('inventory/add', 'add');
 });
 Route::controller(AuthenticationController::class)->group(function () {
     Route::post('auth/login', 'login');
