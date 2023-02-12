8ee74cc6c6852b58aa825cb97a368c09d1fc1598
ellenjane001 <ellenjane.000001998@gmail.com>
2023-02-13 00:56:35 +0800

    added upload image function

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -11,16 +11,16 @@ use Illuminate\Http\Response;
 class ProductsController extends Controller
 {
     //
-    public function add(Request $request)
-    {
-        if ($request->hasFile('product_image')) {
-            $file = $request->file('product_image');
-            $filename = $file->getClientOriginalName();
-            $file->storeAs('product_images', $filename, 's3');
+        public function add(Request $request)
+        {
+            if ($request->hasFile('product_image')) {
+                $file = $request->file('product_image');
+                $filename = $file->getClientOriginalName();
+                $file->storeAs('product_images', $filename, 's3');
+            }
+            DB::insert('INSERT INTO products (product_name,category_id,product_image,product_price) VALUES (?,?,?,?)', [$request->input('product_name'), $request->input('category_id'), Storage::disk('s3')->url('product_images/' . $request->file('product_image')->getClientOriginalName()), $request->input('product_price')]);
+            return response()->json(['success' => true, 'row' => DB::getPdo()->lastInsertId()]);
         }
-        DB::insert('INSERT INTO products (product_name,category_id,product_image,product_price) VALUES (?,?,?,?)', [$request->input('product_name'), $request->input('category_id'), Storage::disk('s3')->url('product_images/' . $request->file('product_image')->getClientOriginalName()), $request->input('product_price')]);
-        return response()->json(['success' => true, 'row' => DB::getPdo()->lastInsertId()]);
-    }
     public function fetch()
     {
         $results =   DB::select('SELECT * FROM products');
