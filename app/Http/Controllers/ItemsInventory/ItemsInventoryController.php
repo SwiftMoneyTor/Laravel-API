7009f97398e3d5f5c64a9d617ace654bc3d4687d
ellenjane001 <ellenjane.000001998@gmail.com>
2023-02-13 17:53:06 +0800

    added inventory functions

--- app/Http/Controllers/ItemsInventory/ItemsInventoryController.php
+++ app/Http/Controllers/ItemsInventory/ItemsInventoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Logs\LogsController;
+use App\Http\Controllers\Products\ProductsController;
 
 class ItemsInventoryController extends Controller
 {
@@ -13,14 +14,26 @@ class ItemsInventoryController extends Controller
     public function fetch()
     {
         $result = DB::select('SELECT * FROM inventory');
-        return response()->json(['success' => true, 'responsedata' => (array)$result[0]]);
+
+        foreach ($result as $res) {
+            $res->item_name = (new ProductsController())->fetchSingleProduct($res->item_id)['product_name'];
+        }
+        return response()->json(['success' => true, 'responsedata' => (array)$result]);
     }
     public function add(Request $request)
     {
-        $result = DB::insert('INSERT INTO inventory (item_name,item_quantity,merchant_id,category_id) VALUES (?,?,?,?)', [$request->input('item_name'), $request->input('item_quantity'), $request->input('merchant_id'), $request->input('category_id')]);
-        if ($result) {
-            $log_id = (new LogsController())->add(['user_id' => 1, 'log_details' => 'added new inventory item', 'id' => DB::getPdo()->lastInsertId(), 'table' => 'inventory']);
-            return response()->json(['success' => true, 'responsedata' => 'successfully added into inventory', 'log_id' => $log_id['log_id']]);
+        $inventory = DB::select('SELECT * FROM inventory WHERE item_id = ?', [$request->input('item_id')]);
+        
+        if (count($inventory) > 0) {
+            $query = DB::update('UPDATE inventory SET item_quantity=item_quantity + ? WHERE item_id = ?', [$request->input('item_quantity'), $request->input('item_id')]);
+        } else {
+            $query = DB::insert('INSERT INTO inventory (item_id,item_quantity,merchant_id,category_id) VALUES (?,?,?,?)', [$request->input('item_id'), $request->input('item_quantity'), $request->input('merchant_id'), $request->input('category_id')]);
+        }
+
+        if ($query) {
+            $log_id = (new LogsController())->add(['user_id' => $request->input('merchant_id'), 'log_details' => 'added new inventory item', 'id' => DB::getPdo()->lastInsertId(), 'table' => 'inventory']);
+            $array_response = ['success' => true, 'responsedata' => 'successfully updated inventory', 'log_id' => $log_id['log_id']];
+            return response()->json($array_response);
         }
     }
     public function edit()
